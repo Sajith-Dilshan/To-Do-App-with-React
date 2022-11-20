0423d5a3bb576e9f424e3b435e9e18f5e351d3ae
sajith <sajithd784@gmail.com>
2022-11-20 16:16:13 +0530

    remove commented thing

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -25,53 +25,6 @@ function TodoList() {
       </div>
     </div>
   );
-
-
-
-  // return (
-  //   <div>
-  //     <h2>My Todos</h2>
-  //     <TodoInput todos={todos} setTodos={setTodos} />
-  //    <div>
-  //    {todos.map((item, index) => {
-  //         return (
-  //           <TodoItem
-              
-  //             key={item.id}
-  //             todo={item}
-  //           />
-  //         );
-  //       })}
-  //    </div>
-  //   </div>
-  // )
 }
-
-{/*
-
-  const [todos, setTodos] = useState([]);
-
-  return (
-    <div className="todo-list-container">
-      <h2>My Todos</h2>
-      <TodoInput todos={todos} setTodos={setTodos} />
-      <div className="todo-items-wrapper">
-        {todos.map((item, index) => {
-          return (
-            <TodoItem
-              todos={todos}
-              setTodos={setTodos}
-              index={index + 1}
-              key={item.id}
-              todo={item}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
-}
-
-*/}
 
 export default TodoList
